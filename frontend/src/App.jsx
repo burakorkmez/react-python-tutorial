d30326b57cf418474ff86f79dc6cb7c71a8a5e21
Burak Orkmez <burakorkmezz@gmail.com>
2024-05-29 02:23:15 +0300

    BASE_URL fixed for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from "./components/Navbar";
 import UserGrid from "./components/UserGrid";
 import { useState } from "react";
 
-export const BASE_URL = "http://127.0.0.1:5000/api";
+// updated this after recording. Make sure you do the same so that it can work in production
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
 
 function App() {
 	const [users, setUsers] = useState([]);
